6d4e1b4068816e1f8953460d73179afbd8b59db4
Andy Scott <andy@spryly.com>
2014-03-06 13:24:20 -0800

    initial commit

---
+++ index.coffee
@@ -0,0 +1,41 @@
+# helpers to convert javascript to and from ASTs in
+# gulp.  - Andy
+
+escodegen = require 'escodegen'
+esprima   = require 'esprima'
+through2  = require 'through2'
+
+exports.parse = -> through2.obj (file, encoding, cb) ->
+  file.ast = esprima.parse String(file.contents)
+  @push file
+  cb()
+
+exports.render = -> through2.obj (file, encoding, cb) ->
+  if file.ast?
+    file.contents = new Buffer(escodegen.generate file.ast)
+    delete file.ast
+  @push file
+  cb()
+
+exports.transform = transform = (f) ->
+  through2.obj (file, encoding, cb) ->
+    if file.ast? then f file.ast
+    @push file
+    cb()
+
+exports.traverse = traverse = (node, cb) ->
+  if Array.isArray node
+    node.forEach (x) ->
+      if x? then traverse x, cb
+  else if node? and typeof node == 'object'
+    cb node    
+    for key of node
+      if node.hasOwnProperty key
+        traverse node[key], cb
+
+exports.rewriteRequire = (f) -> transform (ast) -> traverse ast, (node) ->
+  c = node.callee
+  return unless c? and node.type == 'CallExpression' and c.type == 'Identifier' and c.name == 'require'
+  if node.arguments.length and node.arguments[0].type == 'Literal'
+    mappedName = f node.arguments[0].value
+    if mappedName? then node.arguments[0].value = mappedName
---
+++ index.js
@@ -0,0 +1,76 @@
+var escodegen, esprima, through2, transform, traverse;
+
+escodegen = require('escodegen');
+
+esprima = require('esprima');
+
+through2 = require('through2');
+
+exports.parse = function() {
+  return through2.obj(function(file, encoding, cb) {
+    file.ast = esprima.parse(String(file.contents));
+    this.push(file);
+    return cb();
+  });
+};
+
+exports.render = function() {
+  return through2.obj(function(file, encoding, cb) {
+    if (file.ast != null) {
+      file.contents = new Buffer(escodegen.generate(file.ast));
+      delete file.ast;
+    }
+    this.push(file);
+    return cb();
+  });
+};
+
+exports.transform = transform = function(f) {
+  return through2.obj(function(file, encoding, cb) {
+    if (file.ast != null) {
+      f(file.ast);
+    }
+    this.push(file);
+    return cb();
+  });
+};
+
+exports.traverse = traverse = function(node, cb) {
+  var key, _results;
+  if (Array.isArray(node)) {
+    return node.forEach(function(x) {
+      if (x != null) {
+        return traverse(x, cb);
+      }
+    });
+  } else if ((node != null) && typeof node === 'object') {
+    cb(node);
+    _results = [];
+    for (key in node) {
+      if (node.hasOwnProperty(key)) {
+        _results.push(traverse(node[key], cb));
+      } else {
+        _results.push(void 0);
+      }
+    }
+    return _results;
+  }
+};
+
+exports.rewriteRequire = function(f) {
+  return transform(function(ast) {
+    return traverse(ast, function(node) {
+      var c, mappedName;
+      c = node.callee;
+      if (!((c != null) && node.type === 'CallExpression' && c.type === 'Identifier' && c.name === 'require')) {
+        return;
+      }
+      if (node["arguments"].length && node["arguments"][0].type === 'Literal') {
+        mappedName = f(node["arguments"][0].value);
+        if (mappedName != null) {
+          return node["arguments"][0].value = mappedName;
+        }
+      }
+    });
+  });
+};
